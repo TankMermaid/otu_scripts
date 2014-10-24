8be0c6758934f4c7ad677b6852d27e27a4cee41d
swo <swo@mit.edu>
2014-10-24 17:38:06 -0400

    sort by total ab: improvements

--- sort_by_total_abundance.rb
+++ sort_by_total_abundance.rb
@@ -1,28 +1,33 @@
 #!/usr/bin/env ruby
 #
 # author: scott w olesen <swo@mit.edu>
-#
-# This script reads an OTU table that has a final lineage column. It sorts all the rows
-# based on the sum of all the entries in that row.
+
+require 'arginine'
+
+params = Arginine::parse do 
+  desc "sort rows based on sum of all the entries in that row"
+  flag :index, :desc => "ignore first column?"
+  flag :header, :desc => "ignore first row?"
+  argf "table"
+end
 
 header = ''
 lines = []
 ARGF.each do |line|
-  if $. == 1
+  if $. == 1 and params[:header]
     # this is the header line, just save it
     header = line.chomp
   else
     # for other lines, store the sum of the numeric fields
     fields = line.split
-    otu_id = fields.shift
-    tax = fields.pop
+    idx = fields.shift if params[:index]
     sum = fields.map(&:to_f).inject { |s, x| s + x }
 
     # only keep the line if it's nonzero
-    lines << [sum, line] if sum > 0
+    lines << [sum, line]
   end
 end
 
 # print out the lines in order
-puts header
+puts header if params[:header]
 puts lines.sort { |a, b| b.first <=> a.first }.map(&:last)
