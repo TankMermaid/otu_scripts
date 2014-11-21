170ae75910be31b8323125d62972aa5d319a192a
swo <swo@mit.edu>
2014-11-21 14:37:41 -0500

    slim seq table: improved

--- slim_seq_table.rb
+++ slim_seq_table.rb
@@ -8,13 +8,24 @@ require 'arginine'
 
 params = Arginine::parse do 
   desc "replace actual sequences with seq\#"
+  arg :table, :desc => "output counts table"
+  arg :fasta, :desc => "output fasta"
   argf "seq table"
 end
 
-ARGF.each do |line|
-  if $. == 1
-    puts line.strip
-  else
-    puts line.strip.sub(/[A-Z]+/, "seq#{$. - 1}")
+open(params[:table], 'w') do |table|
+  open(params[:fasta], 'w') do |fasta|
+    ARGF.each do |line|
+      if $. == 1
+        table.write(line)
+      else
+        # find the sequence
+        seq = line.match(/^[A-Z]+/)[0]
+        seq_n = $. - 1
+        seq_name = "seq#{seq_n}"
+        table.write(line.sub(seq, seq_name))
+        fasta.write(">#{seq_name}\n#{seq}\n")
+      end
+    end
   end
 end
