b27d1fe8cbd62c8ae22cb7539c3a32a85fa4e552
swo <swo@mit.edu>
2015-01-26 17:11:59 -0500

    rtra: simplified

--- reads_to_relative_abundances.py
+++ reads_to_relative_abundances.py
@@ -8,24 +8,18 @@ author: scott w olesen (swo@mit.edu)
 goes from counts to relative abundances
 '''
 
-import pysurvey as ps, numpy as np
+import pandas as pd, numpy as np
 import argparse
 
 if __name__ == '__main__':
 	p = argparse.ArgumentParser(description='counts to relative abundances')
 	p.add_argument('table', help='input otu table')
 	p.add_argument('out', help='output otu table')
-	p.add_argument('-p', '--pseudo', type=int, help='add pseudocounts?')
+	#p.add_argument('-p', '--pseudo', type=int, help='add pseudocounts?')
 	args = p.parse_args()
 
 	# read in table
-	table = ps.read_txt(args.table, verbose=False)
+	table = pd.read_table(args.table, index_col=0)
 
-	# normalize
-	if args.pseudo:
-		table = ps.to_fractions(table, method='pseudo', p_counts=arg.pseudo)
-	else:
-		table = ps.to_fractions(table, method='normalize')
-
-	# write the output table
-	ps.write_txt(table, args.out)
+    norm = table.apply(lambda col: col.astype(float) / np.sum(col), axis=0)
+    norm.to_csv(args.out, sep='\t')
