65f8f8ad3d9e02f0f18d7a3b5065bcbf23caed5c
swo <swo@mit.edu>
2015-02-21 13:35:47 -0500

    new jsd matrix script

---
+++ jsd_matrix.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+'''
+author: scott w olesen (swo@mit.edu)
+'''
+
+import numpy as np, pandas as pd
+import argparse, itertools
+
+import warnings
+warnings.filterwarnings("ignore", category = RuntimeWarning)
+
+def jsd(x, y):
+    x = np.array(x)
+    y = np.array(y)
+    d1 = x*np.log2(2*x/(x+y))
+    d2 = y*np.log2(2*y/(x+y))
+    d1[np.isnan(d1)] = 0
+    d2[np.isnan(d2)] = 0
+    d = 0.5*np.sum(d1+d2)    
+    return d
+
+def distance_matrix(table):
+    nrow, ncol = table.shape
+
+    dist = pd.DataFrame(index=table.columns, columns=table.columns)
+
+    for col1, col2 in itertools.combinations(table.columns.values, 2):
+        d = jsd(table[col1], table[col2])
+        dist[col1][col2] = d
+        dist[col2][col1] = d
+
+    return dist
+
+if __name__ == '__main__':
+	p = argparse.ArgumentParser(description='make a jsd matrix')
+	p.add_argument('table', help='normalized otu table')
+	p.add_argument('out', help='output matrix')
+	args = p.parse_args()
+
+	table = pd.read_table(args.table, index_col=0)
+	dist = distance_matrix(table)
+
+	dist.to_csv(args.out, sep='\t')
